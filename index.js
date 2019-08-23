ee9d979760f66d8a3b23baf12b5bc7aef0c479ab
Mantas Kaveckas <mantas.kaveckas@gmail.com>
2019-08-23 14:46:26 +0300

    Exposes native modules

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-
-import { NativeModules } from 'react-native';
+import React, { Component } from "react";
+import {
+  NativeEventEmitter,
+  NativeModules,
+  requireNativeComponent
+} from "react-native";
 
 const { RNAirplay } = NativeModules;
-
-export default RNAirplay;
+export const Airplay = RNAirplay;
+export const AirplayButton = requireNativeComponent("RNAirplayButton", null);
+export const AirplayListener = new NativeEventEmitter(RNAirplay);
